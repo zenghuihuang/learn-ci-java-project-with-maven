0df9608bf15f7348cf159442fc1d993281507ce2
zenghuihuang <huihui127127@gmail.com>
2022-03-01 09:29:53 +0000

    update

---
+++ src/test/java/games/Test1.java
@@ -0,0 +1,14 @@
+package games;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		System.out.println("Hello World! This is my test");
+	}
+
+}
